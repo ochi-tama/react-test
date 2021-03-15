923fa9ba760a18398b8dd18ed4b52adc9f7eade5
ochi-tama <per.raid@gmail.com>
2021-03-15 15:27:39 +0000

    Providerをindex.tsxに移動
    - App.tsxからindex.tsxに移動
    - AuthContextを除去

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
 import React from 'react'
-import { Provider } from 'react-redux'
 import { Redirect, Route, Switch } from 'react-router-dom'
 import styled from 'styled-components'
 import Layout from './components/Layout/Layout'
-import { AuthContext } from './context/AuthContext'
 import Document from './pages/Document/Document'
 import Login from './pages/Login/Login'
 import Search from './pages/Search/Search'
 import Top from './pages/Top/Top'
-import store from './redux/store'
+import { useSelector } from './redux/store'
 
 function App(): JSX.Element {
-  const { currentUser } = React.useContext(AuthContext)
+  const currentUser = useSelector((state) => state.auth.user)
   return (
-    <Provider store={store}>
-      <Switch>
-        <Route exact path="/login">
-          {!!currentUser ? <Redirect to="/" /> : <Login />}
-        </Route>
-        <RootDiv>
-          <Layout authenticated={!!currentUser}>
-            <Switch>
-              <Route exact path="/search" component={Search} />
-              <Route exact path="/documents" component={Document} />
-              <Route component={Top} />
-            </Switch>
-          </Layout>
-        </RootDiv>
-      </Switch>
-    </Provider>
+    <Switch>
+      <Route exact path="/login">
+        {!!currentUser ? <Redirect to="/" /> : <Login />}
+      </Route>
+      <RootDiv>
+        <Layout authenticated={!!currentUser}>
+          <Switch>
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/documents" component={Document} />
+            <Route component={Top} />
+          </Switch>
+        </Layout>
+      </RootDiv>
+    </Switch>
   )
 }
 
